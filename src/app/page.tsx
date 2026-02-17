3891e022c33cf97d57245d49bbdb8bf8bfef1924
Joel Mathias <joelmathias.forever9@gmail.com>
2026-02-17 17:26:57 -0300

    refactor: remove codigo de integração supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,4 @@
-import { supabase } from "@/lib/supabase";
-
 export default async function Home() {
-
-const { data, error } = await supabase
-  .from('User')
-  .delete()
-  .eq('id', 1)
-  .select()
-
   return (
     <main>
     </main>

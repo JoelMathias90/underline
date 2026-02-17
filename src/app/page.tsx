025f7051ce0ee3919ff3656f6fb8ccaf4588a514
Joel Mathias <joelmathias.forever9@gmail.com>
2026-02-17 17:03:46 -0300

    chore: remove arquivos/dependencias prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import { supabase } from "@/lib/supabase";
+
+export default async function Home() {
+
+const { data, error } = await supabase
+  .from('User')
+  .delete()
+  .eq('id', 1)
+  .select()
+
   return (
     <main>
-      <div>Hello world!</div>
     </main>
   );
 }

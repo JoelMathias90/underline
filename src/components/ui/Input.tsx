05c4fee56d7dc8ea228b59b44c98c8bdb644bdd4
Joel Mathias <joelmathias.forever9@gmail.com>
2026-02-26 20:02:54 -0300

    refactor(ui): improve Input Component API

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,26 +1,40 @@
+'use client'
+
 import { InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     label?: string;
     message?: string;
+    error?: boolean;
 }
 
-export default function Input({ label, name, className, message, ...props }: InputProps) {
+export default function Input({
+    name,
+    label,
+    message,
+    error = false,
+    required = true,
+    className = '',
+    ...props
+}: InputProps) {
     return (
-        <label className="" htmlFor={name} >
-            <div>
-                {label}
-            </div>
-            <input {...props}
-                id={name}
-                minLength={8}
-                className={`border border-gray-400 w-full p-1 text-lg ${className}`}
-            />
-            {message &&
-                <p>
-                    {message}
-                </p>
+        <div>
+            {label &&
+                <label htmlFor={name}>
+                    {label}
+                </label>
             }
-        </label>
+            <input
+                id={name}
+                name={name}
+                className={className}
+                {...props}
+            />
+            {message && (
+                <span>
+                    {message}
+                </span>
+            )}
+        </div>
     )
 }

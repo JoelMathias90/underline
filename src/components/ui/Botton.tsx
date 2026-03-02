90b26fa929a98a478d6d44b4fd5db5e55f2ea940
Joel Mathias <joelmathias.forever9@gmail.com>
2026-03-01 21:44:10 -0300

    refactor: changes loading message

--- src/components/ui/Botton.tsx
+++ src/components/ui/Botton.tsx
@@ -27,7 +27,7 @@ export default function Button({
             {...props}
         >
             {loading ? (
-                children + "..."
+                 "Carregando..."
             ) : (
                 children
             )}

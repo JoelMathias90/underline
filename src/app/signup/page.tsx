bb95ec05c86ee67a89bb345cd46c51c6b4b17295
Joel Mathias <joelmathias.forever9@gmail.com>
2026-03-03 00:56:36 -0300

    feat: add registration route

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "@/app/signup/SignupForm";
+
+export default function Signup() {
+    return (
+        <main className="relative grid w-screen h-screen content-center justify-center">
+            <SignupForm/>
+        </main>
+    )
+}

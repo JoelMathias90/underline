122ce97270b7738cf68de47323ec898ed7e8b881
Joel Mathias <joelmathias.forever9@gmail.com>
2026-03-03 22:47:10 -0300

    feat(form): implement handleInputChange and type form state

--- src/app/signup/SignupForm.tsx
+++ src/app/signup/SignupForm.tsx
@@ -1,12 +1,47 @@
 'use client'
 
-import { useActionState } from "react";
+import { ChangeEvent, useActionState, useEffect, useState } from "react";
 import SignupAction from "./actions";
 import Input from "../../components/ui/Input";
 import Button from "../../components/ui/Botton";
 
+type FormDataType = {
+    firstName: string
+    email: string
+    password: string
+    confirmPassword: string
+    acceptedTerms: boolean
+}
+
+const initialFormState: FormDataType = {
+    firstName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    acceptedTerms: false,
+}
+
 export default function SignupForm() {
     const [state, signupAction] = useActionState(SignupAction, null)
+    const [formData, setFormData] = useState<FormDataType>(initialFormState)
+    const isDisabled = !formData.acceptedTerms
+
+    useEffect(() => {
+        setFormData(initialFormState)
+        if(state?.success) console.log(state.success);
+    }, [state])
+    
+
+    function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+        const { name, value, type, checked } = e.target
+        
+        const newValue = type === 'checkbox' ? checked : value
+
+        setFormData({
+            ...formData,
+            [name]: newValue
+        })
+    }
 
     return (
         <form
@@ -24,6 +59,8 @@ export default function SignupForm() {
                 required
                 placeholder="Seu primeiro nome"
                 className="flex box-border min-w-80 min-h-10 border-b-2 focus:outline-none"
+                value={formData.firstName}
+                onChange={e => handleInputChange(e)}
             />
 
             <Input
@@ -32,6 +69,8 @@ export default function SignupForm() {
                 type="email"
                 required
                 placeholder="example@mail.com"
+                value={formData.email}
+                onChange={e => handleInputChange(e)}
                 className="flex box-border min-w-80 min-h-10 border-b-2 focus:outline-none"
             />
 
@@ -41,6 +80,8 @@ export default function SignupForm() {
                 type="password"
                 required
                 placeholder="Digite uma senha"
+                value={formData.password}
+                onChange={e => handleInputChange(e)}
                 className="flex box-border min-w-80 min-h-10 border-b-2 focus:outline-none"
             />
 
@@ -50,14 +91,20 @@ export default function SignupForm() {
                 type="password"
                 required
                 placeholder="Confirme sua senha"
+                value={formData.confirmPassword}
+                onChange={e => handleInputChange(e)}
                 className="flex box-border min-w-80 min-h-10 border-b-2 focus:outline-none"
             />
+
+            {/* Checkbox */}
 
             <label className="flex gap-2 box-border mb-4">
                 <Input
                     name="acceptedTerms"
                     type="checkbox"
                     required
+                    checked={formData.acceptedTerms}
+                    onChange={e => handleInputChange(e)}
                     className="size-4"
                 />
                 <span>
@@ -73,11 +120,12 @@ export default function SignupForm() {
 
             <Button
                 type="submit"
+                disabled={isDisabled}
                 className="flex flex-col justify-center box-border min-w-80 min-h-10 bg-black text-lg text-white hover:bg-gray-700 cursor-pointer"
             >
                 Cadastrar
             </Button>
-            
+
             <div>
                 <p className="text-sm text-center">
                     Já tem uma conta?

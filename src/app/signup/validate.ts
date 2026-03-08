2c810b6c415f840fd523f5b4412b0edc823be19f
Joel Mathias <joelmathias.forever9@gmail.com>
2026-03-07 22:13:25 -0300

    refactor(user-signup-validator): convert class to functional object and improve typings

--- src/app/signup/validate.ts
+++ src/app/signup/validate.ts
@@ -1,46 +1,34 @@
-export type UserFormData = {
-    firstName: string
-    email: string
-    password: string
-    confirmPassword: string
-}
+export const userValidator = {
 
-type FieldValidator<T, F> = (value: T, form: F) => string
-
-type FormValidators<F> = {
-    [K in keyof F]?: FieldValidator<F[K], F>
-}
-
-export const validators: FormValidators<UserFormData> = {
-
-    firstName: (value) => {
-        if (!value.length) return 'Preenchimento obrigatório'
-        if (value.length < 3) return 'Nome muito curto'
-        if (!(/^\p{L}+(?:[ -]\p{L}+)*$/u).test(value)) return 'Válidos somente letras!'
+    firstName(firstName: string): string {
+        if (!firstName.length) return 'Preenchimento obrigatório'
+        if (firstName.length < 3) return 'Nome muito curto'
+        if (!(/^\p{L}+(?:[ -]\p{L}+)*$/u).test(firstName)) return 'Válidos somente letras!'
         return ''
     },
-
-    email: (value) => {
-        if (!value.length) return 'Preenchimento obrigatório'
-        if (!/^.{4,}@/.test(value)) return 'Usuário de email muito curto'
-        if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).test(value)) return "O email deve parecer com example@mail.com"
+    
+    email(email: string): string {
+        if (!email.length) return 'Preenchimento obrigatório'
+        if (!/^.{4,}@/.test(email)) return 'Usuário de email muito curto'
+        if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).test(email)) return "O email deve parecer com example@mail.com"
         return ''
     },
-
-    password: (value) => {
-        if (!value.length) return 'Preenchimento obrigatório'
-        if (value.length < 8) return 'Mínimo de 8 caracteres'
-        if (!/[a-z]/.test(value)) return 'Pelo menos uma letra minúscula'
-        if (!/[A-Z]/.test(value)) return 'Pelo menos uma letra maiúscula'
-        if (!/\d/.test(value)) return 'Pelo menos um número'
-        if (!/[@$!%*?&.#_-]/.test(value)) return 'Pelo menos um caracter especial'
+    
+    password(password: string): string {
+        if (!password.length) return 'Preenchimento obrigatório'
+        if (password.length < 8) return 'Mínimo de 8 caracteres'
+        if (!/[a-z]/.test(password)) return 'Pelo menos uma letra minúscula'
+        if (!/[A-Z]/.test(password)) return 'Pelo menos uma letra maiúscula'
+        if (!/\d/.test(password)) return 'Pelo menos um número'
+        if (!/[@$!%*?&.#_-]/.test(password)) return 'Pelo menos um caracter especial'
         return ''
     },
-
-    confirmPassword: (value, form) => {
-        if (!value.length) return 'Preenchimento obrigatório'
-        if (value !== form.password) return 'As senhas não coincidem'
+    
+    confirmPassword(confirmPassword: string, password: string): string {
+        if (!confirmPassword.length) return 'Preenchimento obrigatório'
+        if (confirmPassword !== password) return 'As senhas não coincidem'
         return ''
     }
-
 }
+
+export type ValidatorField = keyof typeof userValidator
